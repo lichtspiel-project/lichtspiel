5ec958e6b7af2750cdb25e9543cdd8e342173730
Cayoglu <cayoglu@me.com>
2025-03-08 12:54:47 +0100

    Xorshift const generic version

--- src/rng/xorshift.rs
+++ src/rng/xorshift.rs
@@ -6,59 +6,30 @@
 
 use crate::rng::util::u64_to_u32_high;
 
-const XORSHIFT_DEFAULT_A: u64 = 11;
-const XORSHIFT_DEFAULT_B: u64 = 31;
-const XORSHIFT_DEFAULT_C: u64 = 18;
-const XORSHIFT_DEFAULT_TRUNCATE: u64 = 0xd989bcacc137dcd5_u64;
-const XORSHIFT_DEFAULT_STATE: u64 = 0xc1f651c67c62c6e0_u64;
-
-pub struct XorshiftStar32 {
-    a: u64,
-    b: u64,
-    c: u64,
+pub struct GenXorshiftStar<const A: u64, const B: u64, const C: u64, const T: u64> {
     state: u64,
-    threshold: u64,
 }
 
-impl XorshiftStar32 {
-    pub fn new() -> Self {
-        XorshiftStar32 {
-            a: XORSHIFT_DEFAULT_A,
-            b: XORSHIFT_DEFAULT_B,
-            c: XORSHIFT_DEFAULT_C,
-            state: XORSHIFT_DEFAULT_STATE,
-            threshold: XORSHIFT_DEFAULT_TRUNCATE,
-        }
-    }
-
+impl<const A: u64, const B: u64, const C: u64, const T: u64> GenXorshiftStar<A, B, C, T> {
     pub fn with(state: u64) -> Self {
-        XorshiftStar32 {
-            state: state,
-            ..Default::default()
-        }
+        GenXorshiftStar::<A, B, C, T> { state }
     }
-
     fn advance(&mut self) {
-        self.state ^= self.state >> self.a;
-        self.state ^= self.state << self.b;
-        self.state ^= self.state >> self.c;
+        self.state ^= self.state >> A;
+        self.state ^= self.state << B;
+        self.state ^= self.state >> C;
     }
-
     pub fn random(&mut self) -> u32 {
-        let result = self.state.wrapping_mul(self.threshold);
+        let result = self.state.wrapping_mul(T);
         self.advance();
         u64_to_u32_high(result)
     }
-
-    pub fn jump(&mut self, jumps: usize) {
-        for _ in 0..jumps {
-            self.advance();
-        }
-    }
 }
 
-impl Default for XorshiftStar32 {
-    fn default() -> Self {
-        Self::new()
+pub type XorshiftStar32 = GenXorshiftStar<11, 31, 18, 0xd989bcacc137dcd5_u64>;
+
+impl XorshiftStar32 {
+    pub fn new() -> Self {
+        XorshiftStar32::with(0xc1f651c67c62c6e0_u64)
     }
 }
